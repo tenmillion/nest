764a9e5c2c1333e7da2d8b396f69c8cb3e99c103
Yoriko Yamamura <yoriko-y@is.naist.jp>
2014-01-25 19:23:01 +0900

    got the network to kind of work but Im not sure if its correct yet. WB params may be wrong. Output too synced. How much input is threshold?

--- my_brunel2000_rand_hh.py
+++ my_brunel2000_rand_hh.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# brunel2000_rand.py
+# my_brunel2000_rand_hh.py
 #
 # This file is part of NEST.
 #
@@ -21,34 +21,37 @@
 
 import nest
 import nest.raster_plot
+import nest.voltage_trace
 import pylab
 import numpy
 from datetime import datetime
+import sys
 
 # Network parameters. Some of these are given in Brunel (2000) J.Comp.Neuro.
-phi		= 1.1
-g       = 5.0    # Ratio of IPSP to EPSP amplitude: J_I/J_E
-p_rate  = 500.0  # rate of external population in multiples of threshold rate
+phi		= float(sys.argv[1])    # Default 1.
+g       = float(sys.argv[2])    # Ratio of IPSP to EPSP amplitude: J_I/J_E
+p_rate  = float(sys.argv[3])    # rate of external population
 delay   = 1.5    # synaptic delay in ms
-V_init	= -54.402	# Initial membrane potential, set to same value as E_L
+V_init	= -65.	# Initial membrane potential, set to same value as E_L
+E_L	    = -65.
 V_range = 10.	 # Range of initial membrane potential
-
-N_E = 250
+d_range = 0.	# Range of synaptic delay (0 to 1)
+N_E = 200
 N_I = 50
 N_neurons = N_E+N_I
 
 C_E    = N_E/10 # number of excitatory synapses per neuron
 C_I    = N_I/10 # number of inhibitory synapses per neuron  
 
-J_E  = 1.0
+J_E  = 10.
 J_I  = -g*J_E
 
 # Set parameters of the NEST simulation kernel
 nest.SetKernelStatus({"print_time": True,
-                      "local_num_threads": 4})
+                      "local_num_threads": 1})
 tdatetime = datetime.now()
 tstr = tdatetime.strftime('_%m%d-%H%M-%S_')
-fnprefix = "phi"+str('%.1f'%phi)+"g"+str('%.1f'%g)+"in"+str('%.1f'%p_rate)+tstr
+fnprefix = "phi"+str('%.1f'%phi)+"g"+str('%.1f'%g)+"in"+str('%.0f'%p_rate)+tstr
 nest.SetKernelStatus({"data_path": "output", "data_prefix": fnprefix})
 
 # Create and seed RNGs
@@ -58,20 +61,30 @@ pyrngs = [numpy.random.RandomState(s) for s in range(ms, ms+n_vp)]
 nest.SetKernelStatus({'grng_seed': ms+n_vp,
                       'rng_seeds': range(ms+n_vp+1, ms+1+2*n_vp)})
 
-nest.SetDefaults("iaf_psc_delta", {"C_m": 1.0})
-nodes   = nest.Create("iaf_psc_delta",N_neurons)
+# IAF mode for debugging
+#nest.SetDefaults("iaf_psc_delta", {"C_m": 1.0, "tau_m":20*phi})
+#nodes   = nest.Create("iaf_psc_delta",N_neurons)
 
-#nest.SetDefaults("hh_psc_alpha", 
-#                 {"E_L": V_init,
-#				  "phi_t": phi,
-#                  "V_1": 35.0, // Using Wang-Buzsaki
+nest.SetDefaults("hh_psc_alpha", 
+                 {"E_L": -54.402,
+				  "C_m": 100.0,
+				  "phi_t": phi,
+                  "V_1": 40.0, # Using Hodgkin-Huxley
+                  "V_2": 65.0,
+                  "V_3": 65.0,
+                  "V_4": 35.0,
+                  "V_5": 55.0,
+                  "V_6": 65.0})
+
+#                  "E_L": 54.4, # Using Wang-Buzsaki Todo: adjust other E,g for WB too
+#                  "V_1": 35.0, # Using Wang-Buzsaki
 #                  "V_2": 60.0,
 #                  "V_3": 58.0,
 #                  "V_4": 28.0,
 #                  "V_5": 34.0,
 #                  "V_6": 44.0})
 
-#nodes   = nest.Create("hh_psc_alpha",N_neurons)
+nodes = nest.Create("hh_psc_alpha",N_neurons)
 nodes_E= nodes[:N_E]
 nodes_I= nodes[N_E:]
 
@@ -84,15 +97,17 @@ for gid,vp in local_nodes:
 nest.CopyModel("static_synapse", "excitatory")
 for tgt_gid, tgt_vp in local_nodes:
   eweights = pyrngs[tgt_vp].uniform(0.5*J_E, 1.5*J_E, C_E)
+  edelay = pyrngs[tgt_vp].uniform((1-d_range/2.)*delay, (1+d_range/2.)*delay, C_E)
   nest.RandomConvergentConnect(nodes_E, [tgt_gid], C_E,
-                               weight = list(eweights), delay = delay,
+                               weight = list(eweights), delay = list(edelay),
                                model="excitatory")
 
 nest.CopyModel("static_synapse", "inhibitory")
 for tgt_gid, tgt_vp in local_nodes:
   iweights = pyrngs[tgt_vp].uniform(0.5*J_I, 1.5*J_I, C_I)
+  idelay = pyrngs[tgt_vp].uniform((1-d_range/2.)*delay, (1+d_range/2.)*delay, C_I)
   nest.RandomConvergentConnect(nodes_I, [tgt_gid], C_I,
-                               weight = list(iweights), delay = delay,
+                               weight = list(iweights), delay = list(idelay),
                                model="inhibitory")
 
 #nest.CopyModel("static_synapse", "excitatory")
@@ -119,9 +134,9 @@ nest.DivergentConnect(noise,nodes,model="excitatory-input")
 
 spikes=nest.Create("spike_detector",2, 
                    [{"label": "brunel-py-ex", "withtime": True,
-					"withgid": True, "to_file": True},
+					"withgid": True, "to_file": True, "start":2700.},
                     {"label": "brunel-py-in", "withtime": True,
-					"withgid": True, "to_file": True}])
+					"withgid": True, "to_file": True, "start":2700.}])
                    
 spikes_E=spikes[:1]
 spikes_I=spikes[1:]
@@ -130,6 +145,13 @@ N_rec = 50    # Number of neurons to record from
 nest.ConvergentConnect(nodes_E[:N_rec],spikes_E)
 nest.ConvergentConnect(nodes_I[:N_rec],spikes_I)
 
+voltmeter_E = nest.Create("voltmeter")
+voltmeter_I = nest.Create("voltmeter")
+nest.SetStatus(voltmeter_E,[{"to_file": True, "withtime": True, "withgid": True, "start":2700.}])
+nest.SetStatus(voltmeter_I,[{"to_file": True, "withtime": True, "withgid": True, "start":2700.}])
+nest.Connect(voltmeter_E, nodes_E[1:2])
+nest.Connect(voltmeter_I, nodes_I[1:2])
+
 # Visualization of initial membrane potential and initial weight
 # distribution only if we run on single MPI process
 if nest.NumProcesses() == 1:
@@ -137,7 +159,7 @@ if nest.NumProcesses() == 1:
   V_E = nest.GetStatus(nodes_E[:N_rec], 'V_m')
   pylab.hist(V_E, bins=10)
   pylab.xlabel('Membrane potential V_m [mV]')
-  #pylab.savefig('../figures/rand_Vm.eps')
+  pylab.savefig('./figures/'+fnprefix+'rand_Vm.eps')
 
   pylab.figure()
   w = nest.GetStatus(nest.GetConnections(nodes_E[:N_rec],
@@ -146,12 +168,26 @@ if nest.NumProcesses() == 1:
   pylab.hist(w, bins=100)
   pylab.xlabel('Synaptic weight [pA]')
   pylab.title('Distribution of synaptic weights (%d synapses)' % len(w))
-  #pylab.savefig('../figures/rand_w.eps')
+  pylab.savefig('./figures/'+fnprefix+'rand_w.eps')
+
+  pylab.figure()
+  d = nest.GetStatus(nest.GetConnections(nodes_E[:N_rec],
+                                          synapse_model='excitatory'),
+                     'delay')
+  pylab.hist(d, bins=100)
+  pylab.xlabel('Synaptic delay [ms]')
+  pylab.title('Distribution of synaptic delay (%d synapses)' % len(d))
+  pylab.savefig('./figures/'+fnprefix+'rand_d.eps')
 else:
   print "Multiple MPI processes, skipping graphical output"
 
-simtime   = 300.  # how long shall we simulate [ms]
+simtime   = 3000.  # how long shall we simulate [ms]
 nest.Simulate(simtime)
+
+pylab.figure()
+nest.voltage_trace.from_device(voltmeter_E)
+nest.voltage_trace.from_device(voltmeter_I)
+nest.voltage_trace.show()
 
 events = nest.GetStatus(spikes,"n_events")
 
@@ -166,8 +202,10 @@ rate_in= events[1]/simtime*1000.0/N_rec_local_I
 print "Inhibitory rate   : %.2f Hz" % rate_in
 
 if nest.NumProcesses() == 1:
-  nest.raster_plot.from_device(spikes_E, hist=True, title='')
-  #pylab.savefig('../figures/rand_raster.eps')
+  nest.raster_plot.from_device(spikes_E, hist=True, title='Excitatory')
+  pylab.savefig('./figures/'+fnprefix+'rand_raster.eps')
+  nest.raster_plot.from_device(spikes_I, hist=True, title='Inhibitory')
+  pylab.savefig('./figures/'+fnprefix+'rand_raster.eps')
 else:
   print "Multiple MPI processes, skipping graphical output"
 
--- test.py
+++ test.py
@@ -2,25 +2,31 @@
 
 import nest
 import nest.voltage_trace
+import sys
 
 nest.ResetKernel()
 nest.SetKernelStatus({"overwrite_files": True})
 
+V_init	= -65.
+phi = float(sys.argv[1])
+
 nest.SetDefaults("hh_psc_alpha", 
-                 {"phi_t": 1.,
-                  "V_1": 35.,
-                  "V_2": 60.,
-                  "V_3": 58.,
-                  "V_4": 28.,
-                  "V_5": 34.,
-                  "V_6": 44.})
+                 {"E_L": V_init,
+				  "C_m": 100.0,
+				  "phi_t": phi, # More: less tau
+                  "V_1": 35.0, # Using Wang-Buzsaki
+                  "V_2": 60.0,
+                  "V_3": 58.0,
+                  "V_4": 28.0,
+                  "V_5": 34.0,
+                  "V_6": 44.0})
 
 neuron = nest.Create("hh_psc_alpha")
 noise = nest.Create("poisson_generator", 2)
 
 voltmeter = nest.Create("voltmeter")
 
-nest.SetStatus(noise,[{"rate": 80000.0}, {"rate": 15000.0}])
+nest.SetStatus(noise,[{"rate": 130000.0}, {"rate": 10000.0}])
 
 nest.SetStatus(voltmeter,[{"to_file": True, "withtime": True}])
 
