844dfe4c3cec5afdb1627095aa8ac0c642e68e18
Yoriko Yamamura <yoriko-y@naist>
2014-01-29 04:14:42 +0900

    Still tweaking makedb.py

--- makedb.py
+++ makedb.py
@@ -3,12 +3,13 @@ import os
 import glob
 import re
 
+# makedb.py modified for the NEST simulations
 
 # Create table
 if not os.path.isfile('output.db'):
 	conn = sql.connect('output.db')
 	c = conn.cursor()
-	c.execute('''CREATE TABLE output (filename text PRIMARY KEY, temp real, inp real, inb real, pp real, pb real, bp real)''')
+	c.execute('''CREATE TABLE output (filename text PRIMARY KEY, thres int, phi real, g real, iext real, ji real, jis real, ne int, ni int, msyn int, type text, trial int)''')
 	print "Created table"
 else:
 	conn = sql.connect('output.db')
@@ -16,33 +17,46 @@ else:
 	
 # Read filenames from directory and add table entries
 # expected format of output file names:
-# "s-degc%3.1f-inp%3.3f-inb%3.3f-pp%3.5f-pb%3.5f-bp%3.5f-.dat",
-#     celsius, inp,     inb, p2pscale, p2bscale, b2pscale
+# "T0mVphi0.6g4.0in40.0pA_JI-0.100+-0.0_E400I0_MsynII100_0128-1413-41_brunel-py-ex-401-0.txt"
+#     thres    phi,   g,    iext,            ji,      jis    ne,     ni,            msyn,                                     type,  trial
 
 nadded = 0
 
-for x in glob.glob('s-*.dat'):
-	temp_x = re.search('degc([0-9]+\.[0-9]+)',x).group(1)
-	inp_x = re.search('inp([0-9]+\.[0-9]+)',x).group(1)
-	inb_x = re.search('inb([0-9]+\.[0-9]+)',x).group(1)
-	pp_x = re.search('pp([0-9]+\.[0-9]+)',x).group(1)
-	pb_x = re.search('pb([0-9]+\.[0-9]+)',x).group(1)
-	bp_x = re.search('bp([0-9]+\.[0-9]+)',x).group(1)
-
-	entry = [(x, temp_x, inp_x, inb_x, pp_x, pb_x, bp_x),]
+for x in glob.glob('output/*/*/*.txt'):
+	thres_x = re.search('T([0-9]+)mV',x).group(1)
+	phi_x = re.search('phi([0-9]+\.[0-9]+)',x).group(1)		#
+	g_x = re.search('g([0-9]+\.[0-9]+)',x).group(1)
+	iext_x = re.search('in([0-9]+\.[0-9]+)pA',x).group(1)	#
+	ji_x = re.search('JI\-([0-9]+\.[0-9]+)',x).group(1)			#
+	jis_x = re.search('\+\-([0-9]+\.[0-9]+)',x).group(1)
+	ne_x = re.search('E([0-9]+)',x).group(1)						#
+	ni_x = re.search('I([0-9]+)',x).group(1)						#
+	msyn_x = re.search('MsynII([0-9]+)',x).group(1)			#
+	type_x = re.search('brunel\-py\-(ex|in)',x).group(1)		#
+	trial_x = re.search('([0-9]+)\.txt',x).group(1)				#
+	entry = [(x.replace("\\","/"), thres_x, phi_x, g_x, iext_x, ji_x, jis_x, ne_x, ni_x, msyn_x, type_x, trial_x),]
+	print entry
 	
-	try:
-		c.executemany('INSERT INTO output VALUES (?,?,?,?,?,?,?)', entry)
+	c.execute('SELECT COUNT(filename) FROM output WHERE thres=:thres AND phi=:phi AND g=:g AND iext=:iext AND ji=:ji AND\
+						jis=:jis AND ne=:ne AND ni=:ni AND msyn=:msyn AND type=:type AND trial=:trial',\
+						{"thres":thres_x, "phi":phi_x, "g":g_x, "iext":iext_x, "ji":ji_x, "jis":jis_x, "ne":ne_x, "ni":ni_x, "msyn":msyn_x, "type":type_x, "trial":trial_x})
+	entry=c.fetchone()[0]
+
+#	try:
+	if entry==0:
+		c.executemany('INSERT INTO output VALUES (?,?,?,?,?,?,?,?,?,?,?,?)', entry)
 		nadded += 1
-	except:
-		print "Did not add duplicate "+x
+	else:
+		print "Overlapping parameters", x
+#	except:
+#		print "Did not add duplicate", x
 
 conn.commit()
 print "Added", nadded, "files"
 	
-# Print table just created, ordered by temperature, then by inp and inb, then by pp, pb, bp
+# Print table just created, ordered
 print "Entries now in table: "
-for row in c.execute('SELECT * FROM output ORDER BY temp, inp, inb, pp, pb, bp'):
+for row in c.execute('SELECT * FROM output ORDER BY type, ne, ni, iext, ji, msyn, phi, trial'):
 	print row
 	
 conn.close()
--- plotdb.py
+++ plotdb.py
@@ -1,59 +1,82 @@
 # Script to plot from a database of results.
-# Only supports rectangular, evenly-spaced parameter subspaces for now.
-# Considering adding feature to allow arbitrary spacing.
-# Y Yamamura Oct 8-9, 2013
-
-dir = 'Data/'
+# Adapted for the NEST simulation.
+# sample input: python plotdb.py d1 d2 type
+# Y Yamamura Jan 29, 2014
 
 import sqlite3 as sql
+import sys
 import os
 import matplotlib.pyplot as plt
 import numpy as np
 
 # Read file names and param values from DB
-if not os.path.isfile(dir+'output.db'):
-	print 'No output.db found in',dir
+if not os.path.isfile('output.db'):
+	print 'No output.db found in ./'
 	exit()
 	
-conn = sql.connect(dir+'output.db')
+conn = sql.connect('output.db')
 c = conn.cursor()
 
 # DB structure:
-# output (filename text PRIMARY KEY,
-# temp real, inp real, inb real, pp real, pb real, bp real )
+# output (filename text PRIMARY KEY, thres int, 
+#				phi real, g real, iext real, ji real, jis real,
+#				ne int, ni int, msyn int, type text, trial int)
+# Expected variables: phi, iext, ji, msyn
 
 # Plot by any two dimensions of the parameter space
-# Todo: specify params from command line
-
-dim1 = 'temp'
-dim2 = 'bp'
-inp = 0.015
-inb = 0.015
-#temp = 30
-pp = 1
-pb = 0.5
-#bp = 1
-nmda = 0.2
-
-tstop = 500
-ncells = 500
 
 print 'Creating 2D supspace...'
 c.execute('DROP TABLE IF EXISTS t1')
 c.execute('DROP TABLE IF EXISTS t2')
 c.execute('DROP TABLE IF EXISTS t3')
-c.execute('DROP TABLE IF EXISTS t4')
 c.execute('DROP TABLE IF EXISTS subspace')
 
-#---Only need to change this part------
-#c.execute('CREATE TABLE t1 AS SELECT * FROM output WHERE temp='+str(temp))
-c.execute('CREATE TABLE IF NOT EXISTS t1 AS SELECT * FROM output WHERE inp='+str(inp))
-c.execute('CREATE TABLE IF NOT EXISTS t2 AS SELECT * FROM t1 WHERE inb='+str(inb))
-c.execute('CREATE TABLE IF NOT EXISTS t3 AS SELECT * FROM t2 WHERE pp='+str(pp))
-c.execute('CREATE TABLE IF NOT EXISTS t4 AS SELECT * FROM t3 WHERE nmda='+str(nmda))
-c.execute('CREATE TABLE IF NOT EXISTS subspace AS SELECT * FROM t4 WHERE pb='+str(pb))
-#c.execute('CREATE TABLE subspace AS SELECT * FROM t3 WHERE bp='+str(bp))
-#--------------------------------------
+if sys.argv[3] == 'both':
+	ni = 100
+	ne = 400
+	type = 'in' # Todo: plot both in same fig
+	ncells = ni
+
+elif sys.argv[3] == 'ex':
+	ni = 0
+	ne = 400
+	type = 'ex'
+	ncells = ne
+
+else:
+	ni = 100
+	ne = 0
+	type = 'in'
+	ncells = ni
+
+dim1 = sys.argv[1]
+dim2 = sys.argv[2]
+cmd = []
+if (dim1 != 'phi') and (dim2 != 'phi'):
+	phi = 5. #Assume this value if phi is fixed
+	cmd.append('WHERE phi='+str(phi))
+if (dim1 != 'iext') and (dim2 != 'iext'):
+	iext = 1.
+	cmd.append('WHERE iext='+str(iext))
+if (dim1 != 'ji') and (dim2 != 'ji'):
+	ji = 0.1
+	cmd.append('WHERE ji='+str(ji))
+if (dim1 != 'msyn') and (dim2 != 'msyn'):
+	msyn = 100
+	cmd.append('WHERE msyn='+str(msyn))
+
+trial = 0 # Todo: get all trials
+thres = 0 # Todo: variable thres
+tstart = 1000
+tstop = 1300
+
+print cmd
+
+c.execute('CREATE TABLE IF NOT EXISTS t1 AS SELECT * FROM output '+cmd[0])
+c.execute('CREATE TABLE IF NOT EXISTS t2 AS SELECT * FROM t1 '+cmd[1])
+c.execute("CREATE TABLE IF NOT EXISTS subspace AS SELECT * FROM t2 WHERE \
+				ni=:ni AND ne=:ne AND type=:type AND trial=:trial AND thres=:thres",
+				{"ni": ni, "ne": ne, "type": type, "trial": trial, "thres": thres})
 
 # Todo: there can be a problem when different parameter values for d1 or d2
 # were tested in a different context. (The data can add more distinct values
@@ -61,7 +84,7 @@ c.execute('CREATE TABLE IF NOT EXISTS subspace AS SELECT * FROM t4 WHERE pb='+st
 
 ndim1=len(c.execute('SELECT DISTINCT '+dim1+' FROM subspace').fetchall())
 ndim2=len(c.execute('SELECT DISTINCT '+dim2+' FROM subspace').fetchall())
-print 'Will generate', ndim1, 'by', ndim2, 'matrix of scatter plots.'
+print 'Will generate', ndim1, 'by', ndim2, 'matrix of raster plots.'
 print '( dim1 =', dim1, ', dim2=', dim2, ')'
 
 print 'Reading file names...'
@@ -90,19 +113,20 @@ for column in tlist:
 fig = plt.figure() 
 for i in range(ndim1):
 	for j in range(ndim2):
-		try:
-			spikes = np.transpose(np.loadtxt(dir+str(flist[i][j]),dtype='float',skiprows=1,delimiter=' ',usecols=(1,2)))
+#		try:
+			spikes = np.loadtxt(flist[i][j].encode('ascii','ignore'),dtype='float')
+			print spikes[0]
 			ax = fig.add_subplot(ndim2,ndim1,ndim1*j+i+1)
-			ax.scatter(spikes[0],spikes[1],s=1,c='k',marker='.')
+			ax.scatter(spikes[:,1],spikes[:,0],s=1,c='k',marker='.')
 			ax.set_title(tlist[i][j],size='6')
-			ax.axis([0,tstop,0,ncells])
+			ax.axis([tstart,tstop,ne,ne+50]) # Only recorded from 50 cells
 			ax.set_xticklabels([])
 			ax.set_yticklabels([])
-			ax.set_xticks([tstop/5,tstop*2/5,tstop*3/5,tstop*4/5])
-			ax.set_yticks([ncells*4/5])
+			ax.set_xticks([tstart+(tstop-tstart)/5,tstart+(tstop-tstart)*2/5,tstart+(tstop-tstart)*3/5,tstart+(tstop-tstart)*4/5])
+			ax.set_yticks([ne+25])
 			print i, j, str(flist[i][j])
-		except:
-			print i, j, "No file yet"
+#		except:
+#			print i, j, "No file yet, or something wrong with loading or plotting"
 plt.show()
 	
 # conn.commit()
