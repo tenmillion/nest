e8f86b813b03c76a958f5392ae0efbbbd931fa36
Yoriko Yamamura <yoriko-y@naist>
2014-01-29 04:25:06 +0900

    fixed overlap problem

--- makedb.py
+++ makedb.py
@@ -21,6 +21,8 @@ else:
 #     thres    phi,   g,    iext,            ji,      jis    ne,     ni,            msyn,                                     type,  trial
 
 nadded = 0
+nskipped = 0
+ndups = 0
 
 for x in glob.glob('output/*/*/*.txt'):
 	thres_x = re.search('T([0-9]+)mV',x).group(1)
@@ -37,26 +39,27 @@ for x in glob.glob('output/*/*/*.txt'):
 	entry = [(x.replace("\\","/"), thres_x, phi_x, g_x, iext_x, ji_x, jis_x, ne_x, ni_x, msyn_x, type_x, trial_x),]
 	print entry
 	
-	c.execute('SELECT COUNT(filename) FROM output WHERE thres=:thres AND phi=:phi AND g=:g AND iext=:iext AND ji=:ji AND\
+	# Check for existing entries with different file name but same params
+	c.execute('SELECT COUNT(*) FROM output WHERE thres=:thres AND phi=:phi AND g=:g AND iext=:iext AND ji=:ji AND\
 						jis=:jis AND ne=:ne AND ni=:ni AND msyn=:msyn AND type=:type AND trial=:trial',\
 						{"thres":thres_x, "phi":phi_x, "g":g_x, "iext":iext_x, "ji":ji_x, "jis":jis_x, "ne":ne_x, "ni":ni_x, "msyn":msyn_x, "type":type_x, "trial":trial_x})
-	entry=c.fetchone()[0]
-
-#	try:
-	if entry==0:
+	count=c.fetchone()[0]
+	
+	if count==0:
 		c.executemany('INSERT INTO output VALUES (?,?,?,?,?,?,?,?,?,?,?,?)', entry)
 		nadded += 1
 	else:
 		print "Overlapping parameters", x
-#	except:
-#		print "Did not add duplicate", x
+		nskipped += count
 
 conn.commit()
-print "Added", nadded, "files"
 	
 # Print table just created, ordered
-print "Entries now in table: "
-for row in c.execute('SELECT * FROM output ORDER BY type, ne, ni, iext, ji, msyn, phi, trial'):
-	print row
+#print "Entries now in table: "
+#for row in c.execute('SELECT * FROM output ORDER BY type, ne, ni, iext, ji, msyn, phi, trial'):
+#	print row
 	
 conn.close()
+
+print "Added", nadded, "files"
+print "Skipped", nskipped, "files due to overlapping parameters"
